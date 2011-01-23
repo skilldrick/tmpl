160935e2f4ce668cd7bf490eaa110595b671539c
Skilldrick <skilldrick@gmail.com>
2011-01-23 14:10:33 +0000

    Made first template work.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,17 @@
-/* Author: 
+$(document).ready(function () {
+  var data = { title: "Page 1", 
+    list: [ 
+      "This is a list item",
+      "This is also a list item"
+    ]
+  };
 
-*/
+  $('#simple-template').tmpl(data).appendTo('#main');
 
+  /*
+ http://www.borismoore.com/2010/09/introducing-jquery-templates-1-first.html
 
+http://blog.reybango.com/2010/07/09/not-using-jquery-javascript-templates-youre-really-missing-out/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  */
+});
