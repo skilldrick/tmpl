80656a9be028d273ad1284e2423d1ee0d33b6241
Skilldrick <skilldrick@gmail.com>
2011-01-23 14:43:35 +0000

    Made some notes

--- js/script.js
+++ js/script.js
@@ -13,5 +13,18 @@ $(document).ready(function () {
 
 http://blog.reybango.com/2010/07/09/not-using-jquery-javascript-templates-youre-really-missing-out/
 
+  ${expression} renders expression, HTML encoded
+  {{html expression}} renders expression as HTML
+  
   */
 });
+
+//Templates can process arbitrary expressions
+//"But don't go overboard! There is not a complete JavaScript parser
+//in the context of inline expressions in the template. If you have
+//complex code, write a function, and call the function from your
+//template, and pass parameters if you need to: ${myFunction(a,b)}"
+function doSomething(str) {
+  return str.toUpperCase();
+}
+
